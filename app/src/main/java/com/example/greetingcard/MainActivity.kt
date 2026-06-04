7ec1030ae19f85cb38cd2b0334e408c3d61cafa1
Sanjay R <sanjayr1110@gmail.com>
2026-06-04 12:58:16 +0530

    Automated update

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GreetingCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Samsung",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
